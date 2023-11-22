ab214506e9537d4d3abf2846b23d1bdb54255e15
yerin <dpfls1022@naver.com>
2023-11-22 17:33:00 +0900

    Initial Commit

---
+++ Musinsa_Lens/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Musinsa_Lens
+//
+//  Created by 박예린 on 2023/11/20.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
